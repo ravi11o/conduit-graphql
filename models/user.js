56f12ace949457371e33363fa1b8c4d6d6a194ec
ravi11o <ravi.suraj110@gmail.com>
2020-01-31 12:28:37 +0530

    add user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: { type: String, required: true},
+  email: { type: String, required: true, unique: true },
+  username: { type: String, unique: true },
+  password: { type: String, required: true},
+  bio: String,
+  image: String
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if(this.password && this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
+  }
+  next();
+});
+
+userSchema.methods.verifyPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+} 
+
+module.exports = mongoose.model('User', userSchema);
